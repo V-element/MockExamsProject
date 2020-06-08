1a0f00935da0a14dbb6e6340e23d468be8ab746b
Egor <gnevanov.egor@gmail.com>
2020-06-09 00:50:51 +0300

    Initial commit

---
+++ src/main/java/com/gnevanov/models/User.java
@@ -0,0 +1,54 @@
+package com.gnevanov.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String password;
+    private boolean activated;
+
+    public User(String name, String password, boolean activated) {
+        this.name = name;
+        this.password = password;
+        this.activated = activated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
